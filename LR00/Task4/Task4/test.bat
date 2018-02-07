f8527758e501797239e043a174ff0e58ff1c6881
daria-grebneva <greda2598@gmail.com>
2018-02-07 08:20:23 +0300

    LR 00

---
+++ LR00/Task4/Task4/test.bat
@@ -0,0 +1,34 @@
+REM запуск программы без параметров           
+Task4.exe                            
+IF ERRORLEVEL 1 GOTO err                     
+REM запуск программы с одним нечисловым параметром          
+Task4.exe notANumber   
+IF NOT ERRORLEVEL 1 GOTO err
+REM запуск программы с пустой строкой в качестве одного из параметров 
+Task4.exe ""   
+IF NOT ERRORLEVEL 1 GOTO err  
+REM запуск программы с одним числовым параметром (и проверяем результат) 
+Task4.exe -123 > output.txt                                  
+IF ERRORLEVEL 1 GOTO err 
+FC /B output.txt test\-123.txt 
+IF ERRORLEVEL 1 GOTO err  
+REM запуск программы с двумя числовыми параметрами  
+Task4.exe 15 2 > output.txt                          
+IF ERRORLEVEL 1 GOTO err                            
+FC /B output.txt test\15+2=17.txt                        
+IF ERRORLEVEL 1 GOTO err                            
+REM запуск программы с тремя числовыми параметрами  
+Task4.exe 15 2 -8 > output.txt                       
+IF ERRORLEVEL 1 GOTO err                            
+FC /B output.txt test\15+2+-8=9.txt                      
+IF ERRORLEVEL 1 GOTO err
+REM запуск программы с тремя числовыми параметрами  
+Task4.exe 13 + 7 / 8 * 3.8 > output.txt                       
+IF ERRORLEVEL 1 GOTO err                            
+FC /B output.txt test\13+783.8=9.500.txt                      
+IF ERRORLEVEL 1 GOTO err                                                        
+ECHO Program testing succeeded :-)                  
+EXIT                                                
+:err                                                
+ECHO Program testing failed :-(                     
+EXIT
