c2e4bd88849360b64ce863ced221abb2948a72d9
daria-grebneva <greda2598@gmail.com>
2018-02-14 08:26:09 +0300

    Изменена директория к .exe файлу в .bat файле

--- LR_01/replace/test.bat
+++ LR_01/replace/test.bat
@@ -1,41 +1,43 @@
+set PROGRAM="%~1"
+
 REM запуск программы без параметров           
-Debug\replace.exe                            
+%PROGRAM%                            
 IF NOT ERRORLEVEL 1 GOTO err  
 
 REM запуск программы с пустой строкой в качестве одного из параметров 
-Debug\replace.exe ""   
+%PROGRAM% ""   
 IF NOT ERRORLEVEL 1 GOTO err  
 
 REM запуск программы с несуществующим названием input файла
-Debug\replace.exe inputNotExist.txt output.txt "lala" "zuzuz"
+%PROGRAM% inputNotExist.txt output.txt "lala" "zuzuz"
 IF NOT ERRORLEVEL 1 GOTO err  
 
 REM запуск программы с нулевой искомой строкой 
-Debug\replace.exe test\input1.txt output.txt "" "zuzuz"                                  
+%PROGRAM% test\input1.txt output.txt "" "zuzuz"                                  
 IF ERRORLEVEL 1 GOTO err 
 FC /B output.txt test\outputEmptyString.txt 
 IF ERRORLEVEL 1 GOTO err  
 
 REM запуск программы с заменой искомой строки на большую строку 
-Debug\replace.exe test\input1.txt output.txt "ma" "zuzuz"                                  
+%PROGRAM% test\input1.txt output.txt "ma" "zuzuz"                                  
 IF ERRORLEVEL 1 GOTO err 
 FC /B output.txt test\outputMaToZuzuz.txt 
 IF ERRORLEVEL 1 GOTO err  
 
 REM запуск программы с заменой искомой строки на меньшую строку 
-Debug\replace.exe test\input1.txt output.txt "ma" "1"                                  
+%PROGRAM% test\input1.txt output.txt "ma" "1"                                  
 IF ERRORLEVEL 1 GOTO err 
 FC /B output.txt test\outputMaTo1.txt 
 IF ERRORLEVEL 1 GOTO err 
 
 REM запуск программы с заменой искомой строки на нулевую строку 
-Debug\replace.exe test\input1.txt output.txt "ma" ""                                  
+%PROGRAM% test\input1.txt output.txt "ma" ""                                  
 IF ERRORLEVEL 1 GOTO err 
 FC /B output.txt test\outputMaToEmptyString.txt 
 IF ERRORLEVEL 1 GOTO err 
 
 REM запуск программы с заменой искомой строки на нулевую строку 
-Debug\replace.exe test\input2.txt output.txt "1231234" " I'm happy! "                                  
+%PROGRAM% test\input2.txt output.txt "1231234" " I'm happy! "                                  
 IF ERRORLEVEL 1 GOTO err 
 FC /B output.txt test\outputStringOfNumbers.txt 
 IF ERRORLEVEL 1 GOTO err 
