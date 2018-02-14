6f29529ac9d39e38c3f345a76fdfbfd1ea886dbd
daria-grebneva <greda2598@gmail.com>
2018-02-14 11:42:18 +0300

    Правки в коде #4 + работает для всех конфигураций

--- LR_01/replace/test.bat
+++ LR_01/replace/test.bat
@@ -15,31 +15,31 @@ IF NOT ERRORLEVEL 1 GOTO err
 REM запуск программы с нулевой искомой строкой 
 %PROGRAM% test\input1.txt output.txt "" "zuzuz"                                  
 IF ERRORLEVEL 1 GOTO err 
-FC /B output.txt test\outputEmptyString.txt 
+FC output.txt test\outputEmptyString.txt 
 IF ERRORLEVEL 1 GOTO err  
 
 REM запуск программы с заменой искомой строки на большую строку 
 %PROGRAM% test\input1.txt output.txt "ma" "zuzuz"                                  
 IF ERRORLEVEL 1 GOTO err 
-FC /B output.txt test\outputMaToZuzuz.txt 
+FC output.txt test\outputMaToZuzuz.txt 
 IF ERRORLEVEL 1 GOTO err  
 
 REM запуск программы с заменой искомой строки на меньшую строку 
 %PROGRAM% test\input1.txt output.txt "ma" "1"                                  
 IF ERRORLEVEL 1 GOTO err 
-FC /B output.txt test\outputMaTo1.txt 
+FC output.txt test\outputMaTo1.txt 
 IF ERRORLEVEL 1 GOTO err 
 
 REM запуск программы с заменой искомой строки на нулевую строку 
 %PROGRAM% test\input1.txt output.txt "ma" ""                                  
 IF ERRORLEVEL 1 GOTO err 
-FC /B output.txt test\outputMaToEmptyString.txt 
+FC output.txt test\outputMaToEmptyString.txt 
 IF ERRORLEVEL 1 GOTO err 
 
 REM запуск программы с заменой искомой строки на нулевую строку 
 %PROGRAM% test\input2.txt output.txt "1231234" " I'm happy! "                                  
 IF ERRORLEVEL 1 GOTO err 
-FC /B output.txt test\outputStringOfNumbers.txt 
+FC output.txt test\outputStringOfNumbers.txt 
 IF ERRORLEVEL 1 GOTO err 
                          
 
