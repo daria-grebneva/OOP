57fd297167550ec125da9b6e5d1061f91f20eb6e
daria-grebneva <greda2598@gmail.com>
2018-02-21 08:18:50 +0300

    Поправлен .bat файл

--- LR_01/flipbyte/test.bat
+++ LR_01/flipbyte/test.bat
@@ -8,23 +8,19 @@ REM запуск программы с пустой строкой в качестве одного из параметров
 REM %PROGRAM% ""   
 REM IF NOT ERRORLEVEL 1 GOTO err  
 
+REM выход за границы диапазона 
+%PROGRAM% 256 > output.txt                                 
+IF NOT ERRORLEVEL 1 GOTO err
+
+REM передача нечислового параметра 
+%PROGRAM% a > output.txt                                 
+IF NOT ERRORLEVEL 1 GOTO err
+
 REM 6 96 
 %PROGRAM% 6 > output.txt                                 
 IF ERRORLEVEL 1 GOTO err 
 FC output.txt out1.txt 
 IF ERRORLEVEL 1 GOTO err  
-
-REM выход за границы диапазона 
-%PROGRAM% 256 > output.txt                                 
-IF ERRORLEVEL 1 GOTO err 
-FC output.txt out2.txt 
-IF ERRORLEVEL 1 GOTO err
-
-REM передача нечислового параметра 
-%PROGRAM% a > output.txt                                 
-IF ERRORLEVEL 1 GOTO err 
-FC output.txt out3.txt 
-IF ERRORLEVEL 1 GOTO err
 
 REM 200 19 
 %PROGRAM% 200 > output.txt                                 
