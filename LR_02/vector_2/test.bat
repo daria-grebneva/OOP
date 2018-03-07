7bc3edf778cccb0536019fc197d947926d907972
daria-grebneva <greda2598@gmail.com>
2018-03-07 08:44:00 +0300

    Исправлен файл с тестами

--- LR_02/vector_2/test.bat
+++ LR_02/vector_2/test.bat
@@ -1,7 +1,7 @@
 set PROGRAM="%~1"
 
 REM запуск программы без параметров           
-%PROGRAM%                            
+%PROGRAM%                         
 IF NOT ERRORLEVEL 1 GOTO err  
 
 REM запуск программы с пустой строкой в качестве одного из параметров 
@@ -9,22 +9,22 @@ REM запуск программы с пустой строкой в качестве одного из параметров
 IF NOT ERRORLEVEL 1 GOTO err   
 
 REM test 1
-REM %PROGRAM% 12.3 45.2 > out.txt                                  
-REM IF ERRORLEVEL 1 GOTO err 
-REM FC out.txt test\out1.txt 
-REM IF ERRORLEVEL 1 GOTO err   
+%PROGRAM% < test\in1.txt > out.txt                                  
+IF ERRORLEVEL 1 GOTO err 
+FC out.txt test\out1.txt 
+IF ERRORLEVEL 1 GOTO err   
 
 REM test 2
-REM %PROGRAM% test\in2.txt > out.txt                                  
-REM IF ERRORLEVEL 1 GOTO err 
-REM FC out.txt test\out2.txt 
-REM IF ERRORLEVEL 1 GOTO err   
+%PROGRAM% < test\in2.txt > out.txt                                  
+IF ERRORLEVEL 1 GOTO err 
+FC out.txt test\out2.txt 
+IF ERRORLEVEL 1 GOTO err   
 
 REM test 3
-REM %PROGRAM% test\in3.txt > out.txt                                  
-REM IF ERRORLEVEL 1 GOTO err 
-REM FC out.txt test\out3.txt 
-REM IF ERRORLEVEL 1 GOTO err   
+%PROGRAM% < test\in3.txt > out.txt                                  
+IF ERRORLEVEL 1 GOTO err 
+FC out.txt test\out3.txt 
+IF ERRORLEVEL 1 GOTO err   
 
                            
 ECHO Program testing succeeded :-)                  
