a8b5c5616eed41e6aea3efe6f1dceb780431c3e8
daria-grebneva <greda2598@gmail.com>
2018-03-14 08:30:59 +0300

    Правки #4, #7, #9, #10

---
+++ LR_02/vector_2_tests/VectorProcessorTests.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "..\vector_2\vector_2.h"
+
+
+using namespace std;
+
+bool VectorsAreEqual(vector<double> const& x, vector<double> const& y)
+{
+	return x == y;
+}
+
+// Функция ProcessVector
+BOOST_AUTO_TEST_SUITE(ProcessVector_function)
+
+// Создает пустой вектор из пустого вектора
+BOOST_AUTO_TEST_CASE(makes_empty_vector_from_empty_vector)
+{
+	vector<double> emptyVector;
+	ProcessVector(emptyVector);
+	BOOST_CHECK(emptyVector.empty());
+}
+
+// при обработке вектора с одним числом
+BOOST_AUTO_TEST_SUITE(when_processing_a_vector_with_one_number)
+// должна умножить это число на само себя
+BOOST_AUTO_TEST_CASE(should_mul_this_number_on_each_element)
+{
+	vector<double> numbers = { -1 };
+	ProcessVector(numbers);
+
+	BOOST_CHECK(VectorsAreEqual(numbers,
+		{ (-1 * -1) }
+	));
+	// Аналогично следующей инструкции:
+	// BOOST_CHECK(numbers == vector<double>({ (-1 + 3.5), (3.5 + 3.5) }));
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+// при обработке вектора с несколькими элементами
+BOOST_AUTO_TEST_SUITE(when_processing_a_vector_with_several_elements)
+// долженумножить их минимальный эелемент на каждый элемент вектора
+BOOST_AUTO_TEST_CASE(should_add_their_average_to_each_element)
+{
+	vector<double> numbers = { -1.2, 1.2, 2.3, 3.2 };
+	ProcessVector(numbers);
+
+	const double minNumber = -1.2;
+	BOOST_CHECK(VectorsAreEqual(numbers,
+		{ (-1.2 * minNumber), (1.2 * minNumber), (2.3 * minNumber), (3.2 * minNumber) }
+	));
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
