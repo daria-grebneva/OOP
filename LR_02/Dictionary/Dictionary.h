2efaf61289b799c9b1933551295a4f7da6e198dd
daria-grebneva <greda2598@gmail.com>
2018-03-21 10:45:46 +0300

    Задание Dictionary

--- LR_02/Dictionary/Dictionary.h
+++ LR_02/Dictionary/Dictionary.h
@@ -1,5 +1,16 @@
 #pragma once
 
-#include "stdafx.h"
+#include "stdafx.h";
 
-bool IsValidArgumentsCount(int argc)
+using namespace std;
+typedef multimap<string, string> MMAP_STRING_STRING;
+
+const string EXIT_STRING = "...";
+
+bool IsValidArgumentsCount(int argc);
+bool IsFileExist(std::string& fileName);
+void CommunicateWithUser(const string& word, MMAP_STRING_STRING& dictionary, bool& isNewWord);
+bool IsNeedToSave();
+void ReadDictionaryFromFile(const string& inputFileName, MMAP_STRING_STRING& dictionary);
+string FindWordInDictionary(const string& word, MMAP_STRING_STRING& dictionary);
+bool IsNewWord(const string& word, MMAP_STRING_STRING& dictionary);
--- LR_02/Dictionary/main.cpp
+++ LR_02/Dictionary/main.cpp
@@ -1,8 +1,9 @@
+#include "stdafx.h"
 #include "Dictionary.h"
 
 using namespace std;
 
-int main(int argc, wchar_t * argv[])
+int main(int argc, char* argv[])
 {
 	std::locale::global(std::locale("rus"));
 	SetConsoleCP(1251);
@@ -13,7 +14,37 @@ int main(int argc, wchar_t * argv[])
 		cout << "Wrong number of arguments" << endl;
 		return 1;
 	}
+	string inputFileName = argv[1];
+	if (!IsFileExist(inputFileName))
+	{
+		cout << "Dictionary file was not found" << endl;
+		return 1;
+	}
 
+	MMAP_STRING_STRING dictionary;
 
+	ReadDictionaryFromFile(inputFileName, dictionary);
 
+	string word;
+	bool isNewWord = false;
+	stringstream buf;
+	while (getline(cin, word) && (word != EXIT_STRING))
+	{
+		transform(word.begin(), word.end(), word.begin(), ::tolower);
+		CommunicateWithUser(word, dictionary, isNewWord);
+	}
+
+	if (isNewWord)
+	{
+		if (IsNeedToSave())
+		{
+			fstream clear_file(inputFileName, ios::out);
+			ofstream fout(inputFileName);
+			for (const auto& e : dictionary)
+			{
+				fout << e.first << endl;
+				fout << e.second << endl;
+			}
+		}
+	}
 }
